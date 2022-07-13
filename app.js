45b0473e5dcff84ccf5c56d57ba1516d571d48f2
Moses Muiru <muirumoses9@gmail.com>
2022-07-13 15:49:04 +0300

    this is simple profile for nft

---
+++ app.js
@@ -0,0 +1,15 @@
+// this is the a script for hover since with pure css was alittle....
+
+// first call the element fron the dom
+// the view-image
+
+const hoverImage = document.querySelector(".img")
+const img = document.querySelector(".img-container")
+
+
+hoverImage.addEventListener("mouseover", ()=>{
+    img.target.re
+
+
+    console.log("over")
+})
